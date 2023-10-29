9b2661b51bc5197dd693a43437581e63150d9c57
xireiki <xiayinlily@outlook.com>
2023-10-29 21:05:25 +0800

    fix tailscaled.state save

--- customize.sh
+++ customize.sh
@@ -4,8 +4,8 @@ if [ "${ARCH}" != "arm64" ] ; then
   abort "- 不支持的架构，本模块只支持 arm64 架构的设备"
 fi
 
-if [ -f "/data/adb/modules/tailscale/src/tailscale.state" ]; then
+if [ -f "/data/adb/modules/tailscale/src/tailscaled.state" ]; then
   ui_print "检测到 state，正在保存"
-  cp "/data/adb/modules/tailscale/src/tailscale.state" ${MODPATH}/src
+  cp "/data/adb/modules/tailscale/src/tailscaled.state" ${MODPATH}/src
 fi
 
